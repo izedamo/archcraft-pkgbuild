53b2bd25f99a31dff75dfa308110d8f62e97e461
adi1090x <adi1090x@gmail.com>
2021-05-19 06:06:34 +0530

    updated

---
+++ build_pkgs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+## Dirs
+DIR="$(pwd)"
+PKGS=(`ls -p --hide={LICENSE,README.md,build_pkgs.sh}`)
+
+## Script Termination
+exit_on_signal_SIGINT () {
+    { printf "\n\n%s\n" "Script interrupted." 2>&1; echo; }
+    exit 0
+}
+
+exit_on_signal_SIGTERM () {
+    { printf "\n\n%s\n" "Script terminated." 2>&1; echo; }
+    exit 0
+}
+
+trap exit_on_signal_SIGINT SIGINT
+trap exit_on_signal_SIGTERM SIGTERM
+
+# Build packages
+build_pkgs () {
+	local pkg
+
+	{ echo; echo "Building Packages - "; echo; }
+	for pkg in "${PKGS[@]}"; do
+		echo "Building ${pkg}..."
+		cd ${pkg} && makepkg -s
+		mv *.pkg.tar.zst "$DIR"
+		ls --hide=PKGBUILD | xargs -d '\n' rm -r
+		{ cd "$DIR"; echo; }
+	done	
+}
+
+# Execute
+build_pkgs
